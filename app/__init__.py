a20d914c2394224c13d3005345fbf4d9117d89ca
Karmanyaah Malhotra <karmanyaahm@gmail.com>
2021-01-25 18:08:45 -0500

    username basics

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,8 @@
 from flask import Flask
 from config import Config
 
-app = Flask(__name__)
+app = Flask(__name__,
+template_folder='../templates')
 app.config.from_object(Config)
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,14 @@
-from flask import request
+from flask import request, render_template
 from app import app
 
 
-@app.route("/")
+@app.route("/",methods =['GET', 'POST'])
 def home():
-    return "questions and login"
+    username = request.form.get('uname', None)
+    if username:
+        return render_template('index.html', uname=username)
+    else:
+        return render_template('login.html')
 
 
 @app.route("/leaderboard")
