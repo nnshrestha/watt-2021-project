143098afd52e14352cd38a74a1437d8d47451930
Karmanyaah Malhotra <karmanyaahm@gmail.com>
2021-02-03 19:07:18 -0500

    sutffffff

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ import os
 app = Flask(__name__,
 template_folder='../templates')
 app.config.from_object(Config)
-app.secret_key = os.urandom(10)
+app.secret_key = "aaa" #os.urandom(10)
 from app.Points import init
 init()
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from flask import request, render_template, session, redirect, url_for
 from app import app
 from app.PolyLinear import linearPicker
 from app.Points import savePoints, getPoints
-
+from app.PolynomialGenerator import polynomialgen
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -16,24 +16,49 @@ def home():
             ans = float(ans)
             if abs(ans - x) < 0.02:
                 savePoints(10 + getPoints())
-                return linear("correct")
+                return prob("correct")
             else:
-                return linear("incorrect")
+                return prob("incorrect")
                 
         except ValueError:
             return linear("empty")
     elif request.method == "GET":
         if session.get("username",None):
-            return linear()
+            return prob()
             
         return redirect('/login')
 
-def linear(message = None):
-    x, y,prob = linearPicker()
-    session['x'] =x 
-    print(x)
-    return render_template("linear.html", uname=session["username"], prob=prob, message= message, pts=getPoints())
+def prob(message = None):
+    thing = ""
+    if session.get("difficulty", 1):
+        return linear(message)
+    else:
+        return poly(message)
 
+def linear(message = None):
+    thing = ""
+    prob = ""
+    x = 0
+    if session.get("difficulty", 1) == 1:
+        thing = "Solve for x and type x intercept below. Rounding to 2 decimal places."
+        x, y,prob = linearPicker()
+    else:
+        thing = "Solve for smaller root. Rounding to 2 decimal places."
+        x,y, prob = polynomialgen()
+        x = min(x,y)
+    session['x'] =x 
+    
+    return render_template("thing.html", uname=session["username"], prob=prob, message= message, pts=getPoints(), thing = thing)
+
+@app.route("/level", methods = ["GET","POST"])
+def levelSet():
+    if request.method == "POST":
+        try:
+            session["difficulty"] = int(request.form.get("v", 1))
+        except: pass
+        return redirect("/")
+    else:
+        return render_template("difficulty.html")
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
